98a8e94dc7f78d17025d85ba6e4473b04a2e1b1d
stylotylo <tylerlovell@gmail.com>
2022-08-29 14:09:18 -0400

    valid palindrome

--- exercises/LC125_isPalindrome/index.js
+++ exercises/LC125_isPalindrome/index.js
@@ -1,3 +1,20 @@
-function isPalindrome(s) {}
+//Most efficient way is to think of this like a mirror.
+//We can compare the first and last letter, and then move inward.
+function isPalindrome(s) {
+    //sanitize
+    s = s.toLowerCase().replace(/[\W_]/g, '');
+
+    let left = 0;
+    let right = s.length - 1;
+    while (left < right) {
+        if (s[left] !== s[right]) {
+            return false;
+        }
+        left++;
+        right--;
+    }
+    return true;
+
+}
 
 module.exports = isPalindrome;
